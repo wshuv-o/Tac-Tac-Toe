98697d404881866bb3f836aa241b23116582f523
wshuv-o <100506316+wshuv-o@users.noreply.github.com>
2022-04-16 17:26:21 +0600

    Upgrade on assignSign function

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -37,48 +37,63 @@ void assignSign(){
     
         cout<<player1<<" :";
         cin>>n11;
+        jump1:
+        if(n11<1||n11>9){
+            cout<<"Wrong move\n";
+            cout<<player1<<" :";
+            cin>>n11;
+            goto jump1;
+        }else
         coordinate[n11-1]='X';
         board();
 
         cout<<player2<<" :";
         cin>>n21;
-        if(n11==n21){
+        jump2:
+        if(n11==n21||(n21<1||n21>9)){
             cout<<"Wrong move\n";
             cout<<player2<<" :";
             cin>>n21;
-        }
+            goto jump2;
+        }else
         coordinate[n21-1]='O';
         board();
         
 
         cout<<player1<<" :";
         cin>>n12;
-        if(n12==n21||n12==n11){
+        jump3:
+        if(n12==n21||n12==n11||(n12<1||n12>9)){
             cout<<"Wrong move\n";
             cout<<player1<<" :";
             cin>>n12;
-        }
+            goto jump3;
+        }else
         coordinate[n12-1]='X';
         board();
         
 
         cout<<player2<<" :";
         cin>>n22;
-        if(n22==n21||n22==n11||n22==n12){
+        jump4:
+        if(n22==n21||n22==n11||n22==n12||(n22<1||n22>9)){
             cout<<"Wrong move\n";
             cout<<player2<<" :";
             cin>>n22;
-        }
+            goto jump4;
+        }else
         coordinate[n22-1]='O';
         board();
 
         cout<<player1<<" :";
         cin>>n13;
-        if(n13==n21||n13==n11||n13==n12||n13==n22){
+        jump5:
+        if(n13==n21||n13==n11||n13==n12||n13==n22||(n13<1||n13>9)){
             cout<<"Wrong move\n";
             cout<<player1<<" :";
             cin>>n13;
-        }
+            goto jump5;
+        }else
         coordinate[n13-1]='X';
         board();
         winner();
@@ -89,11 +104,13 @@ void assignSign(){
         else{
             cout<<player2<<" :";
             cin>>n23;
-            if(n23==n21||n23==n11||n23==n12||n23==n22||n23==n13){
+            jump6:
+            if(n23==n21||n23==n11||n23==n12||n23==n22||n23==n13||(n23<1||n23>9)){
                 cout<<"Wrong move\n";
                 cout<<player2<<" :";
                 cin>>n23;
-            }
+                goto jump6;
+            }else
             coordinate[n23-1]='O';
             board();
             winner();
@@ -103,11 +120,13 @@ void assignSign(){
             else{
                 cout<<player1<<" :";
                 cin>>n14;
-                if(n14==n21||n14==n11||n14==n12||n14==n22||n14==n13||n14==n23){
+                jump7:
+                if(n14==n21||n14==n11||n14==n12||n14==n22||n14==n13||n14==n23||(n14<1||n14>9)){
                     cout<<"Wrong move\n";
                     cout<<player1<<" :";
                     cin>>n14;
-                }
+                    goto jump7;
+                }else
                 coordinate[n14-1]='X';
                 board();
                 winner();
@@ -117,11 +136,13 @@ void assignSign(){
                 else{
                     cout<<player2<<" :";
                     cin>>n24;
-                    if(n24==n21||n24==n11||n24==n12||n24==n22||n24==n13||n24==n23||n24==n14){
+                    jump8:
+                    if(n24==n21||n24==n11||n24==n12||n24==n22||n24==n13||n24==n23||n24==n14||(n24<1||n24>9)){
                         cout<<"Wrong move\n";
                         cout<<player2<<" :";
                         cin>>n24;
-                    }
+                        goto jump8;
+                    }else
                     coordinate[n24-1]='O';
                     board();
                     winner();
@@ -131,10 +152,12 @@ void assignSign(){
                     else{
                         cout<<player1<<" :";
                         cin>>n15;
-                        if(n15==n21||n15==n11||n15==n12||n15==n22||n15==n13||n15==n23||n15==n14||n15==n24){
+                        jump9:
+                        if(n15==n21||n15==n11||n15==n12||n15==n22||n15==n13||n15==n23||n15==n14||n15==n24||(n15<1||n15>9)){
                             cout<<"Wrong move\n";
                             cout<<player1<<" :";
                             cin>>n15;
+                            goto jump9;
                         }
                         coordinate[n15-1]='X';
                         board();
